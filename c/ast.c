7f6653ec41d099901fcdbddd5017cd7bff0b7a42
Արմեն Բադալյան <armenbadal@example.com>
2016-09-19 08:37:45 +0400

    Կոդ 1

--- c/ast.c
+++ c/ast.c
@@ -3,5 +3,29 @@
 
 #include "ast.h"
 
-
+/**/
+void expression_as_lisp( struct expression* expr, FILE* out )
+{
+  switch( expr->kind ) {
+    case NUMBER:
+	  fprintf(out, "%lf", expr->number);
+	  break;
+    case VARIABLE:
+	  fprintf(out, "%s", expr->ident);
+	  break;
+    case UNRY:
+	  fprintf(out, "(");
+	  if( NEG == expr->oper )
+		fprintf(out,"-");
+	  else if( NOT == expr->oper )
+		fprintf(out,"not");
+	  expression_as_lisp(expr->exo, out);
+	  fprintf(out, ")");
+	  break;
+    case BINARY:
+	  break;
+    case APPLY:
+	  break;
+  }
+}
 
